5f5e6fa084e01dcd945a170c3ec3d1f0d82fcbc2
MoNafea01 <113165914+MoNafea01@users.noreply.github.com>
2023-03-17 23:41:57 +0200

    Update 5.py

--- Python/5.py
+++ Python/5.py
@@ -8,15 +8,13 @@
 x,num = [2, 5, 3, 4, 5 , 2],5
 def min_dist(x,num):
     import random
-    lst_=[]
-    lst_rnd=[]
-    min_=9999
-    f=0
-    if x.count(num)== 1:
+    lst_,lst_rnd=[],[]
+    min_,f = 9999,0
+    if x.count(num) == 1:
          return 0
     while f<2:
         for i in x:
-            if i==num :
+            if i == num:
                 n=random.random()
                 lst_.append(n)
                 lst_rnd.append(n)
@@ -25,5 +23,6 @@ def min_dist(x,num):
         for i in lst_rnd[:(len(lst_rnd))-1]:
                     min_ = min((lst_.index(lst_rnd[lst_rnd.index(i)+1])) - lst_.index(i),min_)
         x[:(len(x)-1)].reverse()
-        f+=1
+        f += 1
     return min_
+print(min_dist(x,num))
